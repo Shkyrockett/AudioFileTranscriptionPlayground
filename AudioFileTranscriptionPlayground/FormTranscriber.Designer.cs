a2d0210255518c12c33804c94fdfe4d54566187f
Shkyrockett <Shkyrockett@gmail.com>
2022-09-29 21:33:25 -0700

    Add project files.

---
+++ AudioFileTranscriptionPlayground/FormTranscriber.Designer.cs
@@ -0,0 +1,147 @@
+﻿// <copyright file="FormTranscriber.Designer.cs" company="Shkyrockett" >
+//     Copyright © 2020 - 2022 Shkyrockett. All rights reserved.
+// </copyright>
+// <author id="shkyrockett">Shkyrockett</author>
+// <license>
+//     Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </license>
+// <summary></summary>
+// <remarks>
+// </remarks>
+
+namespace AudioFileTranscriptionPlayground
+{
+    /// <summary>
+    /// The form transcriber.
+    /// </summary>
+    partial class FormTranscriber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.TextBox textBoxTranscript;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components is not null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.textBoxTranscript = new System.Windows.Forms.TextBox();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFile.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBoxFile.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBoxFile.Location = new System.Drawing.Point(14, 16);
+            this.textBoxFile.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.PlaceholderText = "Select Audio File";
+            this.textBoxFile.Size = new System.Drawing.Size(884, 23);
+            this.textBoxFile.TabIndex = 0;
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSelectFile.Location = new System.Drawing.Point(897, 16);
+            this.buttonSelectFile.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(23, 23);
+            this.buttonSelectFile.TabIndex = 1;
+            this.buttonSelectFile.Text = "…";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.ButtonSelectFile_Click);
+            // 
+            // textBoxTranscript
+            // 
+            this.textBoxTranscript.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTranscript.Location = new System.Drawing.Point(14, 45);
+            this.textBoxTranscript.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxTranscript.Multiline = true;
+            this.textBoxTranscript.Name = "textBoxTranscript";
+            this.textBoxTranscript.PlaceholderText = "Awaiting audio file selection...";
+            this.textBoxTranscript.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxTranscript.Size = new System.Drawing.Size(906, 426);
+            this.textBoxTranscript.TabIndex = 2;
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExecute.Location = new System.Drawing.Point(13, 479);
+            this.buttonExecute.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(907, 27);
+            this.buttonExecute.TabIndex = 3;
+            this.buttonExecute.Text = "Process File";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.ButtonExecute_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 479);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(907, 27);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 4;
+            this.progressBar.Visible = false;
+            // 
+            // FormTranscriber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.textBoxTranscript);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.textBoxFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormTranscriber";
+            this.Text = "Audio Transcription";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormTranscriber_FormClosed);
+            this.Load += new System.EventHandler(this.FormTranscriber_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+    }
+}
+
---
+++ AudioFileTranscriptionPlayground/FormTranscriber.cs
@@ -0,0 +1,314 @@
+﻿// <copyright file="FormTranscriber.cs" company="Shkyrockett" >
+//     Copyright © 2020 - 2022 Shkyrockett. All rights reserved.
+// </copyright>
+// <author id="shkyrockett">Shkyrockett</author>
+// <license>
+//     Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </license>
+// <summary></summary>
+// <remarks>
+// </remarks>
+
+using AudioFileTranscriptionPlayground.Properties;
+using NAudio.Wave;
+using System.Speech.Recognition;
+using System.Text;
+
+namespace AudioFileTranscriptionPlayground
+{
+    /// <summary>
+    /// The form1.
+    /// </summary>
+    public partial class FormTranscriber
+        : Form
+    {
+        //private static bool completed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormTranscriber"/> class.
+        /// </summary>
+        public FormTranscriber()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Forms the transcriber_ load.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void FormTranscriber_Load(object sender, EventArgs e)
+        {
+            textBoxFile.Text = Settings.Default.SelectedFile;
+        }
+
+        /// <summary>
+        /// Forms the transcriber_ form closed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void FormTranscriber_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Settings.Default.SelectedFile = textBoxFile.Text;
+            Settings.Default.Save();
+        }
+
+        /// <summary>
+        /// ButtonSelectFile click event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void ButtonSelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = textBoxFile.Text;
+            switch (openFileDialog1.ShowDialog())
+            {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    textBoxFile.Text = openFileDialog1.FileName;
+                    break;
+                case DialogResult.TryAgain:
+                case DialogResult.Continue:
+                case DialogResult.No:
+                case DialogResult.Cancel:
+                case DialogResult.Abort:
+                case DialogResult.Retry:
+                case DialogResult.Ignore:
+                case DialogResult.None:
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// ButtonExecute click event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private async void ButtonExecute_Click(object sender, EventArgs e)
+        {
+            buttonSelectFile.Enabled = false;
+            buttonExecute.Enabled = false;
+            textBoxFile.Enabled = false;
+            textBoxTranscript.Enabled = false;
+            textBoxTranscript.PlaceholderText = $"Processing: {Path.GetFileName(textBoxFile.Text)}";
+            textBoxTranscript.Text = string.Empty;
+            progressBar.Visible = true;
+
+            //await Task.Run(OldMethod);
+            await Task.Run(NewMethod);
+
+            progressBar.Visible = false;
+            textBoxTranscript.PlaceholderText = "Awaiting audio file selection...";
+            textBoxTranscript.Enabled = true;
+            textBoxFile.Enabled = true;
+            buttonExecute.Enabled = true;
+            buttonSelectFile.Enabled = true;
+        }
+
+        /// <summary>
+        /// News the method.
+        /// </summary>
+        private void NewMethod()
+        {
+            var file = textBoxFile.Text;
+            var path = Path.GetDirectoryName(file);
+            var filename = Path.GetFileNameWithoutExtension(file);
+            var extension = Path.GetExtension(file);
+            var textFile = Path.Combine(path, $"{filename}.txt");
+
+            using SpeechRecognitionEngine recognizer = new();
+            Grammar dictation = new DictationGrammar { Name = "Dictation Grammar" };
+            recognizer.LoadGrammar(dictation);
+
+            if (extension.Equals(".wav", StringComparison.OrdinalIgnoreCase))
+            {
+                textBoxTranscript.PlaceholderText = $"Transcribing {Path.GetFileName(file)} file…";
+                recognizer.SetInputToWaveFile(file);
+                var text = GetText(recognizer).ToString();
+
+                if (textBoxTranscript.InvokeRequired)
+                {
+                    Invoke((MethodInvoker)(() => textBoxTranscript.Text = text));
+                }
+                else
+                {
+                    textBoxTranscript.Text = text;
+                }
+
+                if (File.Exists(textFile))
+                {
+                    switch (MessageBox.Show($"Would you like to overwrite the file {textFile}?", "Overwrite .txt file?", MessageBoxButtons.YesNo))
+                    {
+                        case DialogResult.Yes:
+                            File.Delete(textFile);
+                            break;
+                        default:
+                            return;
+                    }
+                }
+
+                File.WriteAllText(textFile, text);
+            }
+            else if (extension.Equals(".mp3", StringComparison.OrdinalIgnoreCase))
+            {
+                //using var mp3 = new Mp3FileReader(file);
+                //using var stream = WaveFormatConversionStream.CreatePcmStream(mp3);
+                //sre.SetInputToWaveStream(stream);
+
+                string newFile = Path.Combine(path, $"{filename}.wav");
+                if (Path.Exists(newFile))
+                {
+                    switch (MessageBox.Show($"Would you like to overwrite the file {newFile}?", "Overwrite .wav file?", MessageBoxButtons.YesNoCancel))
+                    {
+                        case DialogResult.Yes:
+                            File.Delete(newFile);
+                            break;
+                        case DialogResult.No:
+                            goto SkipConversion;
+                        default:
+                            return;
+                    }
+                }
+
+                textBoxTranscript.PlaceholderText = $"Converting {Path.GetFileName(file)} to {Path.GetFileName(newFile)}…";
+                ConvertMp3ToWav(file, newFile);
+
+            SkipConversion:
+
+                textBoxTranscript.PlaceholderText = $"Transcribing {Path.GetFileName(newFile)} file…";
+                recognizer.SetInputToWaveFile(newFile);
+                var text = GetText(recognizer).ToString();
+
+                if (textBoxTranscript.InvokeRequired)
+                {
+                    Invoke((MethodInvoker)(() => textBoxTranscript.Text = text));
+                }
+                else
+                {
+                    textBoxTranscript.Text = text;
+                }
+
+                if (File.Exists(textFile))
+                {
+                    switch (MessageBox.Show($"Would you like to overwrite the file {textFile}?", "Overwrite .txt file?", MessageBoxButtons.YesNo))
+                    {
+                        case DialogResult.Yes:
+                            File.Delete(textFile);
+                            break;
+                        default:
+                            return;
+                    }
+                }
+
+                File.WriteAllText(textFile, text);
+            }
+        }
+
+        /// <summary>
+        /// Gets the text.
+        /// </summary>
+        /// <param name="recognizer">The recognizer.</param>
+        /// <returns>A StringBuilder.</returns>
+        private static StringBuilder GetText(SpeechRecognitionEngine recognizer)
+        {
+            recognizer.BabbleTimeout = new TimeSpan(int.MaxValue);
+            recognizer.InitialSilenceTimeout = new TimeSpan(int.MaxValue);
+            recognizer.EndSilenceTimeout = new TimeSpan(100000000);
+            recognizer.EndSilenceTimeoutAmbiguous = new TimeSpan(100000000);
+
+            StringBuilder sb = new();
+            while (true)
+            {
+                try
+                {
+                    if (recognizer.Recognize()?.Text is not string recText)
+                    {
+                        break;
+                    }
+
+                    sb.AppendLine(recText);
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+
+            return sb;
+        }
+
+        ///// <summary>
+        ///// Olds the method.
+        ///// </summary>
+        //private void OldMethod()
+        //{
+        //    using SpeechRecognitionEngine recognizer = new();
+        //    // Create and load a grammar.  
+        //    Grammar dictation = new DictationGrammar { Name = "Dictation Grammar" };
+        //    recognizer.LoadGrammar(dictation);
+
+        //    // Configure the input to the recognizer.  
+        //    recognizer.SetInputToWaveFile(textBoxFile.Text);
+
+        //    // Attach event handlers for the results of recognition.  
+        //    recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Recognizer_SpeechRecognized);
+        //    recognizer.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(Recognizer_RecognizeCompleted);
+
+        //    // Perform recognition on the entire file.  
+        //    Console.WriteLine("Starting asynchronous recognition...");
+        //    completed = false;
+        //    recognizer.RecognizeAsync();
+        //}
+
+        //// Handle the SpeechRecognized event.  
+        ///// <summary>
+        ///// Recognizer_S the speech recognized.
+        ///// </summary>
+        ///// <param name="sender">The sender.</param>
+        ///// <param name="e">The e.</param>
+        //private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        //{
+        //    if (e.Result != null && e.Result.Text != null)
+        //    {
+        //        textBoxTranscript.Text += e.Result.Text;
+        //    }
+        //}
+
+        //// Handle the RecognizeCompleted event.  
+        ///// <summary>
+        ///// Recognizer_S the recognize completed.
+        ///// </summary>
+        ///// <param name="sender">The sender.</param>
+        ///// <param name="e">The e.</param>
+        //private void Recognizer_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        //{
+        //    if (e.Error != null)
+        //    {
+        //        Console.WriteLine("  Error encountered, {0}: {1}",
+        //        e.Error.GetType().Name, e.Error.Message);
+        //    }
+        //    if (e.Cancelled)
+        //    {
+        //        Console.WriteLine("  Operation canceled.");
+        //    }
+        //    if (e.InputStreamEnded)
+        //    {
+        //        Console.WriteLine("  End of stream encountered.");
+        //    }
+        //    completed = true;
+        //}
+
+        /// <summary>
+        /// Converts the mp3 to wav.
+        /// </summary>
+        /// <param name="inPath">The _in path_.</param>
+        /// <param name="outPath">The _out path_.</param>
+        private static void ConvertMp3ToWav(string inPath, string outPath)
+        {
+            using Mp3FileReader mp3 = new(inPath);
+            using WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mp3);
+            WaveFileWriter.CreateWaveFile(outPath, pcm);
+        }
+    }
+}
